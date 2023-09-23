8051c746ed4949ba756e3aad03d0c89be3f23965
Antoine Thomas <73194840+antoineThomasCode@users.noreply.github.com>
2023-09-23 14:20:44 +0200

    header mobile

--- index.js
+++ index.js
@@ -5,17 +5,4 @@ console.log('connected')
     video.controls = false;
 
     // Mettre la vidéo en mode plein écran
-    function toggleFullScreen() {
-        if (video.requestFullscreen) {
-            video.requestFullscreen();
-        } else if (video.mozRequestFullScreen) {
-            video.mozRequestFullScreen(); // Firefox
-        } else if (video.webkitRequestFullscreen) {
-            video.webkitRequestFullscreen(); // Chrome, Safari, etc.
-        }
-    }
-
-    // Activer le mode plein écran lors du chargement de la page
-    video.addEventListener("loadedmetadata", function() {
-        toggleFullScreen();
-    });
+    
